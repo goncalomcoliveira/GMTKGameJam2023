9ffae432df7bf16f3e3aa6fbe78b205bd8962623
up202004953 <up202004953@fc.up.pt>
2023-07-09 00:25:58 +0100

    Characters + Action Manager

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,16 +1,24 @@
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 [System.Serializable]
-public abstract class Interaction
+public abstract class Interaction : MonoBehaviour
 {
     public Position position;
+    public bool random;
 
     public abstract int Execute();
     public void Move(Position local, Room room, CharacterMovement movement)
     {
-        Debug.Log("Movingo to " + position);
+        Debug.Log("Moving");
+        if (random)
+        {
+            do {
+                position = new Position(Random.Range(0, room.ROOMSIZE), Random.Range(0, room.ROOMSIZE));
+            } while (room.Matrix[position.x, position.y] is not EmptySpace);
+        }
+
+        Debug.Log("Moving to " + position);
         List<Position> path = Pathfinding.Search(local, position, room);
         
         movement.SetPath(path);
